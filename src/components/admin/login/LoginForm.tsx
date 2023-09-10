8992ed99c0266cacd785505e13391f02658b1f76
alex-con-ruiz <alexconruiz@gmail.com>
2023-09-10 12:33:25 -0300

    FEAT[Admin-login]: fixed scaffolding

--- src/components/admin/login/LoginForm.tsx
+++ src/components/admin/login/LoginForm.tsx
@@ -3,7 +3,7 @@ import { map } from 'nanostores';
 import { useStore } from '@nanostores/react';
 import Cookies from 'universal-cookie';
 import Styles from './styles.module.scss';
-import TextField from '@components/react/TextField/Textfield';
+import TextField from '@components/react/TextField';
 import type { ValidationMethod } from '@components/react/TextField/types';
 
 const inputValues = map<Record<string, any>>({
